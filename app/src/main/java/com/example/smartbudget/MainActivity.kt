af7a15e1478f72f9b62fffed3c42c42e8c785bca
mohamediliasskaddar <kaddar.mohamediliass@etu.uae.ac.ma>
2026-04-07 17:23:27 +0100

    test :  welcome screen, with bg

--- app/src/main/java/com/example/smartbudget/MainActivity.kt
+++ app/src/main/java/com/example/smartbudget/MainActivity.kt
@@ -5,20 +5,54 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.smartbudget.ui.navigation.AppNavHost
 import com.example.smartbudget.ui.navigation.BottomNavBar
+import com.example.smartbudget.ui.navigation.Screen
 import com.example.smartbudget.ui.theme.SmartBudgetTheme
 
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            SmartBudgetTheme {
+//                val navController = rememberNavController()
+//                val currentRoute = navController.currentBackStackEntryFlow.collectAsState(initial = null)
+//
+//                Scaffold(
+//                    bottomBar = { BottomNavBar(navController) }
+//                ) { innerPadding ->
+//                    AppNavHost(
+//                        navController = navController,
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//
+//}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SmartBudgetTheme {
                 val navController = rememberNavController()
+
+                // 🔥 Observe la route actuelle
+                val currentBackStackEntry = navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStackEntry.value?.destination?.route
+
                 Scaffold(
-                    bottomBar = { BottomNavBar(navController) }
+                    bottomBar = {
+                        // ❌ cacher sur Welcome
+                        if (currentRoute != Screen.Welcome.route) {
+                            BottomNavBar(navController)
+                        }
+                    }
                 ) { innerPadding ->
                     AppNavHost(
                         navController = navController,
